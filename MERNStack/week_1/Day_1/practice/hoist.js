e3bc55f4c2552e0145e8cd31ae231ce332d031b0
JASSERBOULILA <bhimlol123456@gmail.com>
2023-12-18 14:25:10 +0100

    week 1 day 1

--- MERNStack/week_1/Day_1/practice/hoist.js
+++ MERNStack/week_1/Day_1/practice/hoist.js
@@ -1,6 +1,7 @@
 // 1 after the rewrite the output will be word
-var hello = 'world';  
+var hello; 
 console.log(hello);
+hello='world'; 
 
 
 // code snippet 2  the predict will be magnet
@@ -20,26 +21,77 @@ function print(){
 console.log(brendan);//the output will be the first value of brendan which is super cool
 
 // the code snippet 4
-// var food = 'chicken';
-// console.log(food);//the output chicken
-// eat();
-// function eat(){
-//     food = 'half-chicken';
-//     console.log(food);//half-chicken
-//     var food = 'gone';
-// }
+var food = 'chicken';
+console.log(food);//the output chicken
+eat();
+function eat(){
+    food = 'half-chicken';
+    console.log(food);//half-chicken
+    var food = 'gone';
+}
 
 // the code snippet 5
-var food="chicken"
+mean(); //mean is not a function
+console.log(food); // food is not defined
 var mean = function() {
-        console.log(food);
-        food = "fish";
-        console.log(food);
+    food = "chicken";
+    console.log(food);
+    var food = "fish";
+    console.log(food);
 }
-mean();
 
-console.log(food);//fish
-console.log(food);//fish
+console.log(food); //food is not defined
+
+// 6
+console.log(genre); // Output: undefined
+var genre = "disco";
+
+rewind();
+
+function rewind() {
+    genre = "rock";
+    console.log(genre); // the output: rock
+    var genre = "r&b";
+    console.log(genre); // the output: r&b
+}
+
+console.log(genre); // the output: disco
+
+
+// 7
+dojo = "san jose";
+console.log(dojo); //the output: san jose
+
+learn();
+
+function learn() {
+    dojo = "seattle";
+    console.log(dojo); // the output: seattle
+    var dojo = "burbank";
+    console.log(dojo); // the output: burbank
+}
+
+console.log(dojo); // the output: seattle
+
+// 8
+console.log(makeDojo("Chicago", 65));
+console.log(makeDojo("Berkeley", 0));
+
+function makeDojo(name, students) {
+    const dojo = {};
+    dojo.name = name;
+    dojo.students = students;
+
+    if (dojo.students > 50) {
+        dojo.hiring = true;
+    } else if (dojo.students <= 0) {
+        dojo = "closed for now"; // you cant change resigne a constant variable
+    }
+
+    return dojo;
+}
+
+
 
 
 
