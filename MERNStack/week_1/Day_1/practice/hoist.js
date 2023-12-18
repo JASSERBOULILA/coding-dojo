e4a97694cbb153af6c3606094f6897fc6d8089f5
JASSERBOULILA <bhimlol123456@gmail.com>
2023-12-18 14:11:08 +0100

    week 1 day 1

---
+++ MERNStack/week_1/Day_1/practice/hoist.js
@@ -0,0 +1,52 @@
+// 1 after the rewrite the output will be word
+var hello = 'world';  
+console.log(hello);
+
+
+// code snippet 2  the predict will be magnet
+var needle = 'haystack';
+test();
+function test(){
+    var needle = 'magnet';
+    console.log(needle);
+}
+
+// code snippet 3
+var brendan = 'super cool';
+function print(){
+    brendan = 'only okay';
+    console.log(brendan);
+}
+console.log(brendan);//the output will be the first value of brendan which is super cool
+
+// the code snippet 4
+// var food = 'chicken';
+// console.log(food);//the output chicken
+// eat();
+// function eat(){
+//     food = 'half-chicken';
+//     console.log(food);//half-chicken
+//     var food = 'gone';
+// }
+
+// the code snippet 5
+var food="chicken"
+var mean = function() {
+        console.log(food);
+        food = "fish";
+        console.log(food);
+}
+mean();
+
+console.log(food);//fish
+console.log(food);//fish
+
+
+
+
+
+
+
+
+
+
