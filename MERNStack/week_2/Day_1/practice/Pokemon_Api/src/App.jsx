a448c02c1fbade71f9ce0060b42b16e2f6386113
JASSERBOULILA <bhimlol123456@gmail.com>
2023-12-25 13:59:04 +0100

    superhero Api

---
+++ MERNStack/week_2/Day_1/practice/Pokemon_Api/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState,useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [fetchSuperHeros, setFetchSuperHeros] = useState([])
+  const fetchAPI = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon/")
+      .then(response => {
+        console.log("RESPONSE :", response);
+        return response.json()
+      })
+      .then(response => {
+        console.log("FETCH API RESPONSE AS JSON : ", response.results)
+        setFetchSuperHeros(response.results)
+      })
+      .catch(error => console.log("FETCH API ERROR :", error))
+  }
+  const handleClick = (apiCall,stateVariable, setStateVariable) => {
+    console.log("State Variable");
+    if(stateVariable.length !=0){
+      setStateVariable([])
+    } else{
+      apiCall()
+    }
+  }
+  return (
+    <fieldset>
+      <legend><h1>🦹🏼🦸🏼‍♀️ SuperHeros API 🦹🏼🦸🏼‍♀️</h1></legend>
+      {/* <button onClick={fetchAPI}>fetch API</button> */}
+      <button onClick={() => handleClick(fetchAPI, fetchSuperHeros, setFetchSuperHeros)}>fetch API</button>
+      <fieldset style={{backgroundColor:"cornflowerblue"}}>
+        <legend>
+          <h4>Fetch Super Heros Array</h4>
+        </legend>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+            </tr>
+          </thead>
+          {fetchSuperHeros.map((hero,idx) => 
+          <tbody>
+          <tr key={idx}>
+            <td>{hero.name}</td>
+          </tr>
+          </tbody>)}
+        </table>
+      </fieldset>
+    </fieldset>
+  )
+}
+
+export default App
