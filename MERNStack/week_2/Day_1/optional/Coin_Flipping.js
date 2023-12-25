23d3bb7e9d951d95b918612975bcbc2d836d0a91
JASSERBOULILA <bhimlol123456@gmail.com>
2023-12-25 16:06:31 +0100

    Coin Flipping

---
+++ MERNStack/week_2/Day_1/optional/Coin_Flipping.js
@@ -0,0 +1,52 @@
+function tossCoin() {
+        return Math.random() > 0.5 ? "heads" : "tails";
+    }
+    
+
+
+function fiveHeads() {
+    return new Promise((resolve, reject) => {
+        // your code here!
+        let headsCount = 0;
+        let attempts = 0;
+        while (headsCount < 5) {
+            attempts++;
+            let result = tossCoin();
+            if (result === "heads") {
+                headsCount++;
+            } else {
+                headsCount = 0;
+            }
+        }
+        if(headsCount!=0){
+            resolve(`It Tooks ${attempts} tries to flip fives 'heads'`);
+        }else{
+            reject(`It Tooks ${attempts} tries to flip fives heads`);
+        }
+    });
+}
+fiveHeads()
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+console.log("When does this run now?");
+
+
+// function fiveHeadsSync() {
+//         let headsCount = 0;
+//         let attempts = 0;
+//         while(headsCount < 5) {
+//             attempts++;
+//             let result = tossCoin();
+//             console.log(`${result} was flipped`);
+//             if(result === "heads") {
+//                 headsCount++;
+//             } else {
+//                 headsCount = 0;
+//             }
+//         }
+//         return `It took ${attempts} tries to flip five "heads"`;
+//     }
+//     console.log( fiveHeadsSync() );
+//     console.log( "This is run after the fiveHeadsSync function completes" );
+
+
