3d77612501ca9a84a31d0e99c8c732fa2b3c2849
JASSERBOULILA <bhimlol123456@gmail.com>
2024-01-25 20:18:27 +0100

    Devices

---
+++ JavaStack/Week1/day3/practice/Device.java
@@ -0,0 +1,16 @@
+public class Device {
+    private int battery;
+
+
+    public Device(){
+        this.battery = 100;
+    }
+
+    public  int getBattery(){
+        return this.battery;
+    }
+
+    public void setBattery(int battery){
+        this.battery = battery;
+    }
+}
---
+++ JavaStack/Week1/day3/practice/DeviceTest.java
@@ -0,0 +1,22 @@
+public class DeviceTest {
+
+
+    public static void main(String[] args){
+        Phone iphone = new Phone();
+        System.out.println("the iphone charger is  : " + iphone.getBattery());
+        iphone.playGame();
+        System.out.println("the iphone charger after playing is : " + iphone.getBattery());
+        iphone.playGame();
+        System.out.println("the iphone charger after second playing  :" + iphone.getBattery());
+        iphone.charge();
+        iphone.playGame();
+        iphone.playGame();
+        iphone.playGame();
+        iphone.playGame();
+        iphone.playGame();
+        System.out.println("the iphone charger :" + iphone.getBattery());
+        if(iphone.getBattery()<=10){
+            System.out.println("Battery Critical");
+        }
+    }
+}
