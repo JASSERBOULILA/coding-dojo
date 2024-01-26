1f1cdf197130cdfb816afec9de3b0aa26447074e
JASSERBOULILA <bhimlol123456@gmail.com>
2024-01-26 13:44:23 +0100

    Dakichi

---
+++ JavaStack/Week1/day4/practice/dakichi/src/main/java/com/dakichi/dakichi/DakichiApplication.java
@@ -0,0 +1,13 @@
+package com.dakichi.dakichi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DakichiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DakichiApplication.class, args);
+	}
+
+}
---
+++ JavaStack/Week1/day4/practice/dakichi/src/test/java/com/dakichi/dakichi/DakichiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dakichi.dakichi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DakichiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
